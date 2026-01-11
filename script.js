b0c00a6666e8b1f929e9b84e3e65acf97e7a2d32
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2026-01-11 12:53:01 -0300

    Actualizo html, css y js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 1',
-    data: [10, 16, 16, 14, 20], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 2',
+    data: [10, 15, 13, 14, 10.6], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5', 'Día 6'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 45',
-    data: [11, 12, 14, 11, 10, 22], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 3',
+    data: [10, 8, 7, 8, 10, 10], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
