29baf7625b34ce8227045d3a913d62aa3b678911
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2026-07-05 23:16:02 -0300

    Subo nuevamente semana 22 y 23

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
     label: 'Km recorridos en Semana 22',
-    data: [12, 8, 10, 18], // Datos de los km recorridos en cada día
+    data: [9, 8, 10, 18], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
